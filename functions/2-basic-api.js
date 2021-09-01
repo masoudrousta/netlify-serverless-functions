bd9651ef0477198a3bf5ad10178c6f538c7cb4fc
masoudrousta <mrousta2010@gmail.com>
2021-09-01 15:58:18 +0100

    CORS fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,5 +1,11 @@
 const items = require('../assets/data')
 
 exports.handler = async (event, context) => {
-  return { statusCode: 200, body: JSON.stringify(items) }
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: JSON.stringify(items),
+  }
 }
